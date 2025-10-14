7549ea2d8bb350957ca1e4e910b94cd63967df9e
Lucio <79063845+luciok1203@users.noreply.github.com>
2025-10-14 20:14:56 +0900

    Donghyun (#10)
    * Delete src directory
    
    * Add files via upload
    
    * Delete package.json
    
    * Add files via upload
    
    * Delete src directory
    
    * Add files via upload
    
    * dhkim
    
    ---------
    
    Co-authored-by: superhan488 <142711175+superhan488@users.noreply.github.com>
    Co-authored-by: luciok1203 <luciokim123@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LoginPage from './LoginPage';
 import SignupPage from './SignupPage';
 
-function App() {
-  return <SignupPage />;
-}
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
