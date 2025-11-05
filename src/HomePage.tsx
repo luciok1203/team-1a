f4bcaaa5783863722fc8a7201e18445232bad3f3
luciok1203 <luciokim123@gmail.com>
2025-11-06 00:29:47 +0900

    beforeGPT

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,9 +1,10 @@
-// LoginPage.tsx
+// HomePage.tsx
 
 import axios, { AxiosError } from 'axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from './AuthContext';
+import { encodeQueryParams } from './EncodeQueryParams';
 import './HomePage.css';
 
 const API_BASE = 'https://api-internhasha.wafflestudio.com';
@@ -32,6 +33,37 @@ const LoginPage = () => {
 
     checkToken();
   }, [token]);
+
+  const [posts, setPosts] = useState();
+
+  interface Params {
+    [key: string]: string[] | boolean | number;
+    positions: string[];
+    domains: string[];
+    isActive: boolean;
+    order: number;
+  }
+
+  const [params, setParams] = useState<Params>({
+    positions: [],
+    domains: [],
+    isActive: false,
+    order: 0,
+  });
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const queryString = encodeQueryParams({ params });
+        const res = await axios.get(`${API_BASE}/api/post?${queryString}`);
+        console.log(res.data);
+      } catch (error) {
+        const err = error as AxiosError;
+        console.error(err.response?.data);
+      }
+    };
+    getPosts();
+  }, [params]);
 
   return (
     <div className="container">
